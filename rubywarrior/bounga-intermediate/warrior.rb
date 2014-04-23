896562a9a88647934f47053f7e1320ecb194bf57
Nicolas Cavigneaux <nico@bounga.org>
2014-04-23 17:36:06 +0200

    Complete level 3

--- rubywarrior/bounga-intermediate/warrior.rb
+++ rubywarrior/bounga-intermediate/warrior.rb
@@ -1,69 +1,87 @@
 class Warrior
-  DIRECTIONS = [:forward, :left, :right, :backward]
+  DIRECTIONS    = [:forward, :left, :right, :backward].freeze
+  CRITIC_HEALTH = 8.freeze
 
   def initialize(warrior)
-    @warrior = warrior
+    @warrior  = warrior
+    @enemies  = []
+    @captives = []
   end
 
   def play_turn
-    rest_if_needed
-    search_and_fight_enemies
-    walk_to_stairs
+    feel_spaces
+    rest_if_needed!
+    rescue_captives!
+    bind_enemy!
+    fight_enemies!
+    walk_to_stairs!
   end
 
   private
 
-  def search_and_fight_enemies
+  def feel_spaces
     DIRECTIONS.each do |direction|
-      return if turn_finished?
+      something = @warrior.feel(direction)
 
-      space = @warrior.feel direction
-
-      if space.enemy?
-        @warrior.attack! direction
+      if something
+        case
+        when something.enemy? || something.unit.is_a?(RubyWarrior::Units::Sludge)
+          @enemies << direction
+        when something.captive?
+          @captives << direction
+        end
       end
     end
   end
 
-  def walk_to_stairs
+  def rest_if_needed!
     return if turn_finished?
 
-    space = @warrior.feel @warrior.direction_of_stairs
-
-    if space.enemy?
-      @warrior.attack! @warrior.direction_of_stairs
-    else
-      @warrior.walk! @warrior.direction_of_stairs
-      @@last_move = @warrior.direction_of_stairs
-    end
-  end
-
-  def rest_if_needed
-    return if turn_finished?
-
-    if @warrior.health < 4
-      if DIRECTIONS.all? { |d| !@warrior.feel(d).enemy? }
+    if @warrior.health <= CRITIC_HEALTH
+      if @enemies.empty?
         @warrior.rest!
       else
-        @warrior.walk! opposite_direction(@@last_move)
+        @warrior.walk! empty_spaces.first
       end
     end
+  end
+
+  def bind_enemy!
+    return if turn_finished?
+
+    if @enemies.count > 2
+      enemy = @enemies.first
+      @warrior.bind!(enemy) unless @warrior.feel(enemy).captive?
+    end
+  end
+
+  def rescue_captives!
+    return if turn_finished?
+
+    if @captives.any?
+      @warrior.rescue! @captives.first
+    end
+  end
+
+  def fight_enemies!
+    return if turn_finished?
+
+    if @enemies.any?
+      @warrior.attack! @enemies.first
+    end
+  end
+
+  def walk_to_stairs!
+    return if turn_finished?
+
+    @warrior.walk! @warrior.direction_of_stairs
   end
 
   def turn_finished?
     @warrior.instance_variable_get("@action")
   end
 
-  def opposite_direction(direction)
-    case direction
-    when :forward
-      :backward
-    when :backward
-      :forward
-    when :right
-      :left
-    when :left
-      :right
-    end
+  def empty_spaces
+    DIRECTIONS - @enemies - @captives
   end
 end
